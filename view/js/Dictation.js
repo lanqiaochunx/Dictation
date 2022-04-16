1a3e58e933d66fe47b7eb47efc274184f64e7ede
lanqiaochunx <94211412+lanqiaochunx@users.noreply.github.com>
2022-04-16 13:16:49 +0800

    Add files via upload

---
+++ view/js/Dictation.js
@@ -0,0 +1,26 @@
+function Cor(dom){
+	this.render = function(){
+		dom.innerHTML = dom.innerHTML.replace(/\[([^\[\]]*)\]/g,"<input type='text' class='input form-control' data-toggle='popover' data-trigger='focus' title='答案' data-content='$1'></input>");
+	};
+	this.check = function(){
+		var ra = 0;
+		var total = $(".input").length;
+		$(".input").each(function (){
+			var value = $(this).val();
+			var answer = $(this).attr("data-content");
+			if(value == answer) {
+				ra = ra + 1;
+				$(this).css("background-color","lightgreen");
+			}
+			else {
+				$(this).css("background-color","rgb(241,156,178)");
+			}
+		});
+		return parseFloat((ra / total)*100).toFixed(2);
+	};
+	this.answer = function(a){
+		if(a==true){
+			$('[data-toggle="popover"]').popover();   
+		}
+	};
+}
